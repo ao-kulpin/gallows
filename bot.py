d63e59575df5af60d73b39fd672a34b3e757c53a
Alexander Kulpin <alexander.kulpin7@gmail.com>
2024-05-08 18:24:13 +0300

    cleanup

--- bot.py
+++ bot.py
@@ -110,8 +110,8 @@ async def main():
 
     logger.put(text.logBotStart.format(wordCount = data.allRussWords.size))
 
-    await bot.delete_webhook(drop_pending_updates=True)  # drop_pending_updates=True
-    await dp.start_polling(bot, handle_as_tasks=True)  ############ handle_as_tasks=True
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, handle_as_tasks=True)
 
 class TermException(Exception):
     def __init__(self, sigNum: int) :
