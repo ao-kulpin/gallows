4fae01e3a4a9fa41471a791b4e3e2e16f3bd2207
Alexander Kulpin <alexander.kulpin7@gmail.com>
2024-07-19 16:45:31 +0300

    --proxy option

--- bot.py
+++ bot.py
@@ -20,6 +20,8 @@ import gmessage as gm
 import logger
 import useractor
 import botactor
+import sys
+import argparse
 
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.exceptions import TelegramBadRequest
@@ -28,13 +30,42 @@ from aiogram.filters.callback_data import CallbackData
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from aiogram.client.session.aiohttp import AiohttpSession
 
 from config_reader import config
 
-bot = Bot(token=config.bot_token.get_secret_value())
-dp = Dispatcher()
-dp.include_routers(useractor.router, botactor.router)
-logging.basicConfig(level=logging.INFO)
+
+print(f"args: {sys.argv}")
+bot = None  # the bot object
+dp = None   # the dispatcher
+
+async def createBot():    
+    global bot
+    global dp
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--proxy", help="use Pythonanywhere proxy server", 
+                        action="store_true")
+    args = parser.parse_args()
+
+    print("Bot Gallows starting...")
+
+    if args.proxy:
+        # use the proxy server
+        proxyURL = "http://proxy.server:3128"
+        session  = AiohttpSession(proxy=proxyURL)
+        bot = Bot(token=config.bot_token.get_secret_value(), session=session)
+        print(f"using proxy {proxyURL}")
+    else:
+        # start without a proxy
+        bot = Bot(token=config.bot_token.get_secret_value())
+
+    dp = Dispatcher()
+    dp.include_routers(useractor.router, botactor.router)
+    logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    asyncio.run(createBot())
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -102,15 +133,27 @@ async def cmd_exit(message: types.Message, state: FSMContext):
 async def user_away(callback: types.CallbackQuery, state: FSMContext):
     await botExit(callback.message, state)
 
+async def createBot():    
+    global bot
+    global dp
+
+    print("Bot Gallows starting...")
+    proxyURL= 'http://proxy.server:3128'
+    #session = AiohttpSession(proxy=proxyURL)
+    #bot = Bot(token=config.bot_token.get_secret_value(), session=session)
+    bot = Bot(token=config.bot_token.get_secret_value())
+    dp = Dispatcher()
+    dp.include_routers(useractor.router, botactor.router)
+    logging.basicConfig(level=logging.INFO)
+
 # Start the bot
 async def main():
-    print("Bot Gallows starting...")
-
     data.loadDictionary()
 
     logger.put(text.logBotStart.format(wordCount = data.allRussWords.size))
 
     await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot, handle_as_tasks=True)
 
 class TermException(Exception):
